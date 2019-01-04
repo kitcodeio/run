9394f1c25cc45644006ae6a2a68b5c62507bf139
Satnam Singh <singhsandhusatnam@outlook.com>
2019-01-05 03:15:11 +0530

    added image build api based on socket

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 'use strict';
 
 const Routes = require('./lib/routes');
+const socket = require('./lib/socket');
 
 const env = process.env.NODE_ENV || 'beta';
 
 exports.register = async function(plugin, options, next) {
   const config = options.config;
+  const io = require('socket.io')(plugin.listener);
+  io.on('connection', socket);
   plugin.route(Routes(config));
   return next();
 }
