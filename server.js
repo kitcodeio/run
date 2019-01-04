478ac05b6b9cbd47957fd58a00dfba3e29fbb904
Satnam Singh <singhsandhusatnam@outlook.com>
2019-01-05 03:24:25 +0530

    server.js file added

---
+++ server.js
@@ -0,0 +1,22 @@
+const Hapi = require('hapi');
+
+const env = process.env.NODE_ENV || 'beta';
+
+const plugin = require('./index')
+const config = require('./config/config.json')[env];
+
+const server = new Hapi.Server();
+server.connection({
+  host: config.server.run.host,
+  port: config.server.run.port,
+  routes: {
+    cors: true
+  }
+});
+
+plugin.register(server, {
+  config: config
+}, async function() {
+  await server.start();
+  console.log('kitcode api server is online at http://' + config.server.run.host + ':' + config.server.run.port);
+});
