e6ee8078ca3902474bfe8ade7b77083b7664f785
Satnam Singh <singhsandhusatnam@outlook.com>
2019-01-15 14:22:50 +0530

    fixed socket; added remove image function

--- index.js
+++ index.js
@@ -1,19 +1,16 @@
 'use strict';
 
 const Routes = require('./lib/routes');
-const socket = require('./lib/socket');
-const package = require('./package.json');
+const Socket = require('./lib/socket');
 const env = process.env.NODE_ENV || 'beta';
 
 exports.register = async function(plugin, options, next) {
-    let config = options.config;
-    let io = require('socket.io')(plugin.listener);
-
-    io.on('connection', new socket);
-    plugin.route(Routes(config));
-    return next();
+  let config = options.config;
+  new Socket();
+  plugin.route(Routes(config));
+  return next();
 }
 
 exports.register.attributes = {
-    pkg: package
+  pkg: require('./package.json')
 }
