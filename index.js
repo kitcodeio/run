d098fb923a5205a366927a6cfd05cbb9906bcb58
Satnam Singh <singhsandhusatnam@outlook.com>
2019-02-03 18:09:28 +0530

    added ubuntu-nginx as base image

--- index.js
+++ index.js
@@ -3,10 +3,13 @@
 const Routes = require('./lib/routes');
 const Socket = require('./lib/Socket');
 
+const ubuntuNginx = require('./lib/image-checker');
+
 exports.register = async function(plugin, options, next) {
   let config = options.config;
   new Socket(config, plugin.listener);
   plugin.route(Routes(config));
+  ubuntuNginx.check(config.docker.baseImageName);
   return next();
 }
 
